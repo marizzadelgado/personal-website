820e2e3570c9531edabb9f87bae4edcabcf427cb
Marizza <rizza1020@gmail.com>
2020-05-06 19:34:17 -0700

    added src files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App.js';
+import './fonts/Raleway-Regular.ttf'
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
