0d3c37558db508805d52e9df294f35f398fcd651
Marizza <rizza1020@gmail.com>
2020-05-17 16:33:06 -0700

    deploy again lol

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/marizzadelgado.github.io/precache-manifest.d9b2b0f689344efbcc0cde1452f9a959.js"
+  "/marizzadelgado.github.io/public/precache-manifest.97771739fe4e3256c684feca956eedab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marizzadelgado.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marizzadelgado.github.io/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
