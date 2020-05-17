4a0c77c9a31de1b150cd7413cbba16211c37eed6
Marizza <rizza1020@gmail.com>
2020-05-16 22:42:57 -0700

    added slideshow

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import logo from '/Users/rafaeladelgado/myfirstreact/src/images/logo.png';
+import logo from './../src/images/logo.png';
 import ImageGallery from 'react-image-gallery';
 
-import linkedin from '/Users/rafaeladelgado/myfirstreact/src/images/linkedin.png';
-import facebook from '/Users/rafaeladelgado/myfirstreact/src/images/facebook.png';
-import instagram from '/Users/rafaeladelgado/myfirstreact/src/images/instagram.png';
-import youtube from '/Users/rafaeladelgado/myfirstreact/src/images/youtube.png';
+import linkedin from './../src/images/linkedin.png';
+import facebook from './../src/images/facebook.png';
+import instagram from './../src/images/instagram.png';
+import youtube from './../src/images/youtube.png';
 
-import idewcare from '/Users/rafaeladelgado/myfirstreact/src/images/idewcare.jpg';
-import fitbit from '/Users/rafaeladelgado/myfirstreact/src/images/fitbit.png';
-import refinery29 from '/Users/rafaeladelgado/myfirstreact/src/images/refinery29.jpg';
-import loreal from '/Users/rafaeladelgado/myfirstreact/src/images/loreal.jpg';
-import fenty from '/Users/rafaeladelgado/myfirstreact/src/images/fenty.png';
-import drjart from '/Users/rafaeladelgado/myfirstreact/src/images/drjart.png';
+import stars from './../src/images/stars.png';
+import marilyn from './../src/images/marilyn.png';
+import industry from './../src/images/industry.jpg';
 
-
-import stars from '/Users/rafaeladelgado/myfirstreact/src/images/stars.png';
-import marilyn from '/Users/rafaeladelgado/myfirstreact/src/images/marilyn.png';
-import industry from '/Users/rafaeladelgado/myfirstreact/src/images/industry.jpg';
-
-import highlights from '/Users/rafaeladelgado/myfirstreact/src/images/highlights.jpg';
+import highlights from './../src/images/highlights.jpg';
 
 console.log(logo);
  
@@ -38,16 +30,16 @@ class Info extends React.Component {
 			<div className = "info">	
 			<div className = "socialMedia">
 				<a href="https://www.linkedin.com/in/marizza-delgado/">
-				<img src= {linkedin} alt="LinkedIn" width="20" height="20" border="0"/></a>
+				<img src= {linkedin} alt="LinkedIn" width="30" height="30" /></a>
 				
 				<a href="https://www.facebook.com/Marizzapizza/">
-				<img src= {facebook} alt="Facebook" width="20" height="20" border="0" /></a>
+				<img src= {facebook} alt="Facebook" width="30" height="30" /></a>
 				
 				<a href="https://www.instagram.com/marizzadelgado/?hl=en">
-				<img src= {instagram} alt="Instagram" width="20" height="20" border="0" /></a>
+				<img src= {instagram} alt="Instagram" width="30" height="30"  /></a>
 				
 				<a href="https://www.youtube.com/channel/UCkisg0LuWSop4eMy7G3ZBKA">
-				<img src= {youtube} alt="Youtube" width="20" height="20" border="0" /></a>
+				<img src= {youtube} alt="Youtube" width="30" height="30"  /></a>
 			</div>
 			
 			<div className = "bio">
@@ -61,8 +53,55 @@ class Info extends React.Component {
 	}
 } 
 
-	
+
+ 
 class Slideshow extends React.Component {
+  render() {
+  	const images = [
+  		{
+			original: 'https://i.ytimg.com/vi/ECdKH_NRS7c/maxresdefault.jpg',
+			thumbnail: 'https://static.wayup.com/company_logo/ojTomB0qG4_20150310.png'
+  		},
+  		{
+			original: 'https://pbs.twimg.com/media/EEM2jNzXsAA9_mT.jpg',
+			thumbnail: 'https://brandslogo.net/wp-content/uploads/2016/01/fitbit-logo-vector-download-400x400.jpg',
+  		},
+  		{
+			original: 'https://mediaslide-us.storage.googleapis.com/industry/pictures/1302/3678/large-1551202982-dd57fa1ed474d79a1dcca944308f0c0d.jpg',
+			thumbnail: 'https://i.vippng.com/png/small/139-1392591_the-cut-logo-new-york-magazine-the-cut.png',
+  		},
+  		{
+			original: 'https://hips.hearstapps.com/vidthumb/0b4afd89-a826-4baf-a524-fa516b6048ec/thumb_1920x1080_00001_1559872562_21729.jpg',
+			thumbnail: 'https://www.handelgroup.com/wp-content/uploads/2015/10/cosmopolitan-square.jpg',
+  		},
+  		{
+			original: 'https://www.art-for-hair-davos.ch/wp-content/uploads/2018/05/Color-Addict-Shimmer-by-LOreal-Professionnel.jpg',
+			thumbnail: 'https://cdn.worldvectorlogo.com/logos/l-oreal-professionnel.svg',
+  		},
+  		{
+			original: 'https://images.summitmedia-digital.com/preview/images/2019/02/12/ALICEOLIVIA-nm.jpg',
+			thumbnail: 'https://yt3.ggpht.com/a/AGF-l79_4RMz4be2o8ipOXnypIwyn-sZsrMlqpzVmg=s900-c-k-c0xffffffff-no-rj-mo',
+  		},
+  		{
+			original: 'https://i.ytimg.com/vi/HNinVGNGUR0/maxresdefault.jpg',
+			thumbnail: 'https://www.signedbybeauty.com/images/dr_jart_squareimage.jpg',
+  		},
+  		{
+			original: 'https://www.fentybeauty.com/on/demandware.static/-/Sites-FENTY-Library/default/dw64098b45/pdp-content/day-to-night-model.jpg',
+			thumbnail: 'https://cdn.clipart.email/8c2d203776cd0a56e7a501d068609a74_fenty-beauty-logos_768-768.jpeg',
+  		},
+	];
+    return (
+    	<div className = "slideshowContainer">
+    	<ImageGallery items={images} />
+    	</div>
+    );
+  }
+}
+
+
+
+/* class Slideshow extends React.Component {
 	render() {
 		return (
 			<div className= "slideshowContainer">
@@ -102,14 +141,15 @@ class Slideshow extends React.Component {
 		);
 	}
 } 
+*/
 
 class Agencies extends React.Component{
 	render() {
 		return(
-			<div className = "agencies">
-			Represented by:
+			<div className = "agencies" >
+			<h2>Represented by:</h2>
 			<a href="https://www.starsmanagement.com/portfolio/standard/Marizza_Delgado/8516/14353">
-			<img src={stars} alt="stars" height = "60" width= "170" /></a>
+			<img src={stars} alt="stars" height = "60" width= "170"   /></a>
 			<a href="http://www.marilynagency.com/new-york/women/women/marizza-delgado">
 			<img src={marilyn} alt="marilyn" height = "90" width= "70" /></a>
 			<a href="https://www.theindustrymodelmgmt.com/model/los-angeles/management/1302-marizza-delgado/">
@@ -126,8 +166,9 @@ class Highlights extends React.Component {
 			<div className = "highlightsPhoto">
 				<img src={highlights} alt="alt photo" height = "400" width= "400" />
 			</div>
-			<div className = "funFacts">
+			<div className = "funFactsContainer">
 				<h1>Highlights</h1>
+				<div className = "funFacts">
 				<ul>
 				<li>Vegan for 2+ years. Follow my vegan <a href ="https://www.instagram.com/healthy_ho/">food instagram</a>! #BeautyWithoutCruelty </li>
 				<li>Crowned <a href= "https://becrownedproductions.com/miss-pacific-coast">Miss Pacific Coast Teen USA 2017</a>- a preliminary for Miss California Teen USA. </li>
@@ -137,7 +178,8 @@ class Highlights extends React.Component {
 				<li>Shared my skincare journey with my favorite Korean Skincare blog- <a href = "https://theklog.co/skin-care-ingredients-for-smooth-skin-texture/">The Klog</a>!</li>
 				<li>Self proclaimed book worm- let’s connect on <a href ="https://www.goodreads.com/user/show/109858824-marizza-delgado">Goodreads</a>! </li> 
 				</ul>
-				<h1>Booking Inquiries</h1>
+				</div>
+				<h2>Booking Inquiries</h2>
 				<p><a href="mailto:kristen@starsmanagement.com">kristen@starsmanagement.com</a></p>
 			</div>
 			</div>
