7a26dae83a2624580a763fd7c27bca25147450a8
Marizza <rizza1020@gmail.com>
2020-05-17 16:02:56 -0700

    5/17 deploy

---
+++ build/precache-manifest.d9b2b0f689344efbcc0cde1452f9a959.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7186507b0fed21560ed3620be919ef10",
+    "url": "/marizzadelgado.github.io/index.html"
+  },
+  {
+    "revision": "a9ae5c4ee6684ffde02c",
+    "url": "/marizzadelgado.github.io/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "a14b882a9fe380965d7f",
+    "url": "/marizzadelgado.github.io/static/css/main.c33abbb5.chunk.css"
+  },
+  {
+    "revision": "a9ae5c4ee6684ffde02c",
+    "url": "/marizzadelgado.github.io/static/js/2.a718b8a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marizzadelgado.github.io/static/js/2.a718b8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a14b882a9fe380965d7f",
+    "url": "/marizzadelgado.github.io/static/js/main.ac2ceb53.chunk.js"
+  },
+  {
+    "revision": "ef917f286ea400d3b1e6",
+    "url": "/marizzadelgado.github.io/static/js/runtime-main.bc460563.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/marizzadelgado.github.io/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "52f111d8b5afd098d9e1f6dab8f2a8d6",
+    "url": "/marizzadelgado.github.io/static/media/highlights.52f111d8.jpg"
+  },
+  {
+    "revision": "999e0adaf438428ab67fdeebf3481d4a",
+    "url": "/marizzadelgado.github.io/static/media/industry.999e0ada.png"
+  },
+  {
+    "revision": "cdf853ae4d4a8e9e1e619be8ffa7bc06",
+    "url": "/marizzadelgado.github.io/static/media/instagram.cdf853ae.png"
+  },
+  {
+    "revision": "f98b1a44b1903442942390969214c826",
+    "url": "/marizzadelgado.github.io/static/media/logo.f98b1a44.png"
+  },
+  {
+    "revision": "60d07f7f51e3b4b9aeb263e5e154c3d3",
+    "url": "/marizzadelgado.github.io/static/media/marilyn.60d07f7f.png"
+  }
+]);
